86bc06073386498dafea368561dc03eec9b47675
Raven Jiang <raven@cs.stanford.edu>
2017-06-30 00:06:00 -0700

    Added sample config.js

---
+++ config.sample.js
@@ -0,0 +1,16 @@
+// Make a copy of this file to ./config.js and fill up the API keys
+
+module.exports = {
+  twitter: {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: '',
+  },
+  gdax: {
+    url: 'https://api.gdax.com',
+    key: '',
+    secret: '',
+    passphrase: '',
+  },
+};
